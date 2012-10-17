503a40bbae5e2cb0c47024e6c399292ce12c8ebe
Laura Semesky <lsemesky@gmail.com>
2012-10-17 07:26:39 -0400

    Populating species type, be sure to run db:migrate

--- db/migrate/20121017111358_populateanimaltypes.rb
+++ db/migrate/20121017111358_populateanimaltypes.rb
@@ -1,12 +1,11 @@
 class Populateanimaltypes < ActiveRecord::Migration
 def up
-    types = ['Lion','Panda','Koala']
-    for type in types
-        AnimalType.create(:species=>type)
-    end
+
+      AnimalType.create([{species: 'Lion'},{species: 'Panda'},{species: 'Koala'}])
+
   end
 
   def down
-    State.delete_all
+    AnimalType.delete_all
   end
 end
