a86c4025d1797888283cf1e238ece147d9056336
Laura Semesky <lsemesky@gmail.com>
2012-10-31 15:37:14 -0400

    sample user and animal types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'a', email: 'a@a.com', password: 'aaaaaa')
+AnimalType.create([{ species: 'Lion' }, { species: 'Panda' },{species: 'Koala'}])
