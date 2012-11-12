d132938fbaf07bf8c8234bd38d548191ccc65c70
Laura Semesky <lsemesky@gmail.com>
2012-11-12 15:27:53 -0500

    merging sixiao's changes

--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -1,4 +1,4 @@
 class Zoo < ActiveRecord::Base
   has_many :animals
-  attr_accessible :address, :email, :name, :phone
+  attr_accessible :address, :email, :name, :phone, :ed, :belongs, :remarks, :zoo_id, :animal_id
 end
