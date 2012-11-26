16aadeb58f1ab1c4324eb1c40391b049689381e3
Laura Semesky <lsemesky@gmail.com>
2012-11-26 01:44:35 -0500

    added validations, children and sibling functions

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,32 +1,74 @@
 class Animal < ActiveRecord::Base
-  
- # before_save :is_valid_parent
-  
+
   belongs_to :zoo
- # belongs_to :animal_type
- # has_ancestry
   validates :breeding_status, :gender, :dob, :animal_type, :presence => true
   attr_accessible :ancestry, :breeding_status, :comments, :dob, :gender, :name, :animal_type, :zoo_id, :mother_id, :father_id
-  
- 
 
   has_many :fathered_children, :class_name => "Animal", :foreign_key => :father_id
   has_many :mothered_children, :class_name => "Animal", :foreign_key => :mother_id
-  belongs_to :father, :class_name => "Animal" 
-  belongs_to :mother, :class_name => "Animal" 
-  
-  def has_valid_parents
-    mother = Animal.find(mother_id)
-    father = Animal.find(father_id)
-    if animal_type != mother.animal_type || animal_type != father.animal_type
-      errors = "parent type does not match animal type"
-      return false
-    end
-    if mother.gender != 'female' || father.gender != 'male'
-      errors = "parent gender mismatch"
-      return false
-    end
-    return true
+   belongs_to :father, :class_name => "Animal"
+   belongs_to :mother, :class_name => "Animal"
+
+ before_save :set_parents
+ validate :correct_parent_gender, :parents_share_species
+ 
+ def correct_parent_gender()
+   unless mother_id.blank?
+     if mother.gender == 'male'
+       errors.add :base, 'trying to assign male animal to mother'
+     end
+   end
+   
+   unless father_id.blank?
+     if father.gender == 'female'
+       errors.add :base, 'trying to assign female animal to father'
+     end
+   end  
+ end
+ 
+ def parents_share_species()
+   unless mother_id.blank?
+      new_mother = Animal.find(mother_id)
+      if new_mother.animal_type != self.animal_type
+        errors.add :base, 'mother species does not match child species'
+      end
+   end
+   
+   unless father_id.blank?
+     new_father = Animal.find(father_id)
+     if new_father.animal_type != self.animal_type
+        errors.add :base, 'father species does not match child species'
+     end
+   end   
+ end
+ 
+ def set_parents()
+   unless mother_id.blank?
+      new_mother = Animal.find(mother_id)
+      self.mother = new_mother
+   end
+   
+   unless father_id.blank?
+     new_father = Animal.find(father_id)
+     self.father = new_father
+   end
+ end
+ 
+
+  def children
+    if self.gender == 'male'
+      fathered_children
+    elsif self.gender == 'female'
+      mothered_children
+    else
+      nil
+    end   
+  end
+
+  def siblings
+    ((self.father ? self.father.children : []) +
+    (self.mother ? self.mother.children : [])
+    ).uniq - [self]
   end
 
 end
